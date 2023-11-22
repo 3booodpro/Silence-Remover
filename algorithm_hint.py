abf62e4e1f3c6870e405ce1f82c9816c1adee020
3boood_pro <apoda980@gmail.com>
2023-11-22 19:14:14 +0300

    main

---
+++ algorithm_hint.py
@@ -0,0 +1,27 @@
+import auto_editor.edit as edit
+import auto_editor.ffwrapper as ffwrapper
+import auto_editor.validate_input as validator
+import auto_editor.utils.func as edit_funcs
+import auto_editor.utils.log as edit_log
+
+my_args = edit.Args(input=["test.wav"]) # set out input
+
+# setting ffmpeg to use it later (there's nothing fancy here..)
+ffmpeg = ffwrapper.FFmpeg(
+        my_args.ffmpeg_location,
+        my_args.my_ffmpeg,
+        my_args.show_ffmpeg_commands,
+        my_args.show_ffmpeg_output,
+    )
+
+#the same thing again
+temp = edit_funcs.setup_tempdir(my_args.temp_dir, edit_log.Log())
+
+#the same thing again
+log = edit_log.Log(my_args.debug, my_args.quiet, temp=temp)
+log.debug(f"Temp Directory: {temp}")
+
+#validating our file first
+paths = validator.valid_input(my_args.input, ffmpeg, my_args, log)
+#start the editing process
+edit.edit_media(paths, ffmpeg, my_args, temp, log)
