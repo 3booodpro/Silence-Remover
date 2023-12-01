504bed480452fe3a0569b5e705f622cefb446420
KaramA199 <weprozx@gmail.com>
2023-12-01 15:47:13 +0300

    Requested changes
    Added new page with progress bar and related messages and a 'main menu' button.
    
    NOTE: 2 "errors" need fixing:
    1) "Next button" should appear after the user has chosen an export type...
    i didn't know how to do it :(
    2) I tried to play around with the title colors.. i think we can make it more visually pleasing this way. However, the title words get farther apart when you change the app's size.

--- UIPrototype.py
+++ UIPrototype.py
@@ -4,14 +4,15 @@ from PIL import Image, ImageTk
 from math import *
 from time import *
 from threading import *
+
+
 set_appearance_mode("dark")
 root = CTk()
-root.title("Test")
+root.title("Silence Remover")
 root.geometry("1280x720")
 
 #EXPLORER 
 def select_file():
-    global count
     file_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4;*.avi"), ("Audio Files", "*.mp3;*.wav")])
     if file_path:
         deletebutton.pack(side=LEFT,fill=BOTH)
@@ -20,47 +21,116 @@ def select_file():
         size_kb = file_size / 1024
         size_mb = size_kb / 1000
         Namesize.configure(text=f"\n         Selected File:   {file_name}\nFile Size:   {size_mb:.0f} MB")
-        nextab.place(relx = 0.5, rely = 0.7, anchor = CENTER)
+        export5.place(relx = 0.5, rely = 0.55, anchor= CENTER)
+        nextab.place(relx = 0.5, rely = 0.65, anchor = CENTER)
+
+
+
+
+
+
 
 
 def delete_selection():
-    deletebutton.pack_forget()   # Hide the Delete button
-    nextab.place_forget()     # Hide the Next button
+    deletebutton.pack_forget()   # Hide the buttons
+    nextab.place_forget()     
     Namesize.configure(text="")
+    export5.place_forget()
 
-      
+
+
 
 def next_page():
-    label.place_forget()
     file_select_button.pack_forget()
     deletebutton.pack_forget()
     nextab.place_forget()
     Namesize.place_forget()
     frame.place_forget()
+    export5.place_forget()
+    
+    global progress_frame
+    progress_frame = CTkFrame(root, width = 700, height = 100, fg_color='transparent')
+    progress_frame.place(relx = 0.5, rely= 0.45, anchor= CENTER)
+    global progress_label
+    progress_label = CTkLabel(progress_frame, text="Cutting...", font = ("Aldo the Apache", 26))
+    progress_label.place(relx = 0.5, rely = 0.25, anchor = CENTER)
+    global progress_bar
+    progress_bar = CTkProgressBar(progress_frame, mode="indeterminate", progress_color='#3C9E6D')
+    progress_bar.start()  
+    progress_bar.place(relx = 0.5, rely = 0.6, anchor= CENTER)
+    root.after(8500, lambda: stopbar(progress_frame, progress_bar))
 
 
+def mainmenu():
+    backtomenu.place_forget()
+    finish_label.place_forget()
+    progress_label.place_forget()
+    progress_frame.place_forget()
+    file_select_button.pack(side=LEFT,fill=BOTH,expand=True)
+    frame.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+    
+
+
+def stopbar(progress_frame, progress_bar):
+    global backtomenu
+    global finish_label
+    progress_bar.place_forget()
+    progress_label.configure(text = "Done!")
+    finish_label = CTkLabel(root,text = "File has been saved at the same directory of this application", font = (("Aldo the Apache", 20)))
+    finish_label.place(relx = 0.5, rely = 0.45, anchor = CENTER)
+    progress_label.configure(text_color = "#3C9E6D")
+
+    image4 = CTkImage(light_image=Image.open("img14.png"), size=(30,30))
+    backtomenu = CTkButton(root, text= "Return to Main Menu", text_color='white', fg_color='transparent', font = ("Aldo the Apache", 26), image=image4, hover_color='#3C9E6D', command=mainmenu)
+    backtomenu.place(relx = 0.5, rely = 0.55, anchor = CENTER)
 
 
 
 canvas = CTkCanvas(root,bg="#242424",highlightthickness=0)
 canvas.pack(fill=BOTH,expand=TRUE)
+
 Namesize = CTkLabel(root, text = "", font = ("Aldo the Apache", 16))
-Namesize.place(relx = 0.5, rely = 0.55, anchor = "center")
-label = CTkLabel(root,text="Silence Remover",font=("Aldo The Apache", 42))
-label.place(relx=0.5,rely=0.4,anchor=CENTER)
-frame = CTkFrame(root,width=256,height=32,border_width=32,border_color="")
-frame.place(relx=0.5,rely=0.5,anchor=CENTER)
+Namesize.place(relx = 0.5, rely = 0.45, anchor = "center")
+
+label = CTkLabel(root,text="Silence",font=("Aldo The Apache", 46), text_color='#e3e3e3')
+label2 = CTkLabel(root,text="Remover",font=("Aldo The Apache", 46), text_color='#0099cc')
+label.place(relx=0.44,rely=0.3,anchor=CENTER)
+label2.place(relx=0.56,rely=0.3,anchor=CENTER)
+
+
+frame = CTkFrame(root,width=256,height=36,border_width=32,border_color="")
+frame.place(relx=0.5,rely=0.4,anchor=CENTER)
 frame.propagate(0)
+
+export5 = CTkComboBox(root, values= ['Select export type','mp4', 'mp3'], width= 256, border_color="#484A48", button_color="#484A48", border_width=2, fg_color='#2C2C2C', 
+                     text_color='white', font= ("Aldo the Apache", 16), dropdown_font=("Aldo the Apache", 12), state= "readonly", hover=True)
+
+
+   
+
+
+#export
+
+export5.lift(aboveThis=frame)
+export5.bind("<<ComboboxSelected>>")
 
 
 
 image2 = CTkImage(light_image= Image.open("img11.png"), size = (30,30))
 # file_entry = CTkEntry(frame,border_width=0)
 # file_entry.pack(side=LEFT,fill=BOTH,expand=True,anchor=CENTER)
-file_select_button = CTkButton(frame,text="Select file",fg_color="transparent",width=36, image = image2, font = ("Aldo The Apache", 18), text_color= 'white', command = select_file, hover_color='gray')
+file_select_button = CTkButton(frame,text="Select file",fg_color="transparent",width=36, image = image2,
+                                font = ("Aldo The Apache", 18), text_color= 'white', command = select_file, hover_color='#5d91a2'
+                                , border_color='#859da8',border_width=float(0.7))
 file_select_button.pack(side=LEFT,fill=BOTH,expand=True)
 
-deletebutton = CTkButton(master=frame, text="X", fg_color="transparent", width=36, text_color= 'white' ,hover_color='gray', font=("Aldo The Apache", 18), command = delete_selection)
+imagedel = CTkImage(light_image= Image.open("img13.png"), size = (25,25))
+deletebutton = CTkButton(master=frame, text="X", fg_color="transparent", width=36, text_color= 'white' ,
+                         hover_color='#9E3C3C', font=("Aldo The Apache", 18), command = delete_selection, image = imagedel
+                         ,border_width=1, border_color='#9E3C3C')
+
+
 
 previous_time = time()
 image = Image.open("BG.png")
@@ -71,7 +141,8 @@ offset_y = 0
 
 
 image3 = CTkImage(light_image= Image.open("img12.png"), size = (50,50))
-nextab = CTkButton(root, text = "Next", image = image3, fg_color='transparent', width=16, hover_color='gray', border_color='gray', border_width=1, font = ("Aldo The Apache", 18), command= next_page,)
+nextab = CTkButton(root, text = "Cut it!", image = image3, fg_color='transparent', width=16, 
+                   hover_color='#4b8b60', border_color='#3C9E6D', border_width=1, font = ("Aldo The Apache", 18), command= next_page)
 
 def process():
     global previous_time, image, speed, offset_x, offset_y
