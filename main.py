83a37fed123b702b77ca96919a1264a2b9ce639d
3boood_pro <apoda980@gmail.com>
2023-12-15 02:20:41 +0300

    Connecting backend with frontend

--- main.py
+++ main.py
@@ -1,140 +1,176 @@
-import customtkinter as ctk
-from tkinter import filedialog, Canvas
-import os
+from customtkinter import *
 from PIL import Image, ImageTk
-from tkinter import messagebox
-import threading
-import time
+from math import *
+from time import *
+from threading import *
+from editor import *
 
+set_appearance_mode("dark")
+root = CTk()
+root.title("Silence Remover")
+root.geometry("1280x720")
 
-
-
-#main window
-ctk.set_default_color_theme('green')
-ctk.set_appearance_mode('light')
-window = ctk.CTk()
-window.title('Silence Remover')
-window.geometry('1024x720')
-
-
-
-
-def click1():
-    messagebox.showinfo("Info", "This app can cut the silence parts out of videos or audio files. \nSelect a video or audio file you would like to cut the silence out of.")
-
-#img1 info on click
-image_path = os.path.join(os.path.dirname(__file__), 'images/7.png')
-image = ctk.CTkImage(light_image= Image.open(image_path), size = (50,50))
-image_label = ctk.CTkLabel(window, image = image, text = "")
-image_label.place(x = 10, y = 10)
-Button1 = ctk.CTkButton(window, command = click1, height = 0, width= 0, image = image, fg_color= 'transparent', text = "", hover_color="green")
-Button1.place(x = 10, y = 10)
-
-
-#frame
-font = ctk.CTkFont(family= "Gabriola", size = 25)
-frame = ctk.CTkFrame(window)
-frame.pack(padx = 70, pady = 25, fill="both", expand = True)
-
-#darkmode toggle
-def changeMode():
-    val = switch.get()
-    if val:
-        ctk.set_appearance_mode('dark')
-    else:
-        ctk.set_appearance_mode('light')
-
-switch = ctk.CTkSwitch(master= frame, text="Dark mode", onvalue= 1, offvalue= 0, command= changeMode, font = font)
-switch.place(relx = 0.9, rely=0.9, anchor="center")
-
-
-
-
-#windows explorer select file
-apps =[]
+#EXPLORER 
 def select_file():
+    global file_path
     file_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4;*.avi"), ("Audio Files", "*.mp3;*.wav")])
     if file_path:
+        deletebutton.pack(side=LEFT,fill=BOTH)
         file_name = file_path.split("/")[-1]  # Extract the file name from the path
         file_size = os.path.getsize(file_path)
         size_kb = file_size / 1024
         size_mb = size_kb / 1000
-        Text.configure(text=f"\nSelected File:   {file_name}\nFile Size:   {size_mb:.2f} MB")
- 
+        Namesize.configure(text=f"\n         Selected File:   {file_name}\nFile Size:   {size_mb:.0f} MB")
+        export5.place(relx = 0.5, rely = 0.55, anchor= CENTER)
+        nextab.place(relx = 0.5, rely = 0.65, anchor = CENTER)
 
-            
+def delete_selection():
+    deletebutton.pack_forget()   # Hide the buttons
+    nextab.place_forget()     
+    Namesize.configure(text="")
+    export5.place_forget()
 
+def next_page():
+    if file_path == "":
+        return
 
-
-# SHOW APP NAME & SIZE BELOW select file (test)
-Text = ctk.CTkLabel(frame, text = "", font = font)
-Text.place(relx = 0.5, rely = 0.38, anchor = "center")
-
-SELECTFile = ctk.CTkButton(frame, height = 50, width= 80, text = 'Choose a file', command = select_file, font = font)
-SELECTFile.place(relx = 0.5, rely = 0.3, anchor = "center")
-
+    file_select_button.pack_forget()
+    deletebutton.pack_forget()
+    nextab.place_forget()
+    Namesize.place_forget()
+    frame.place_forget()
+    export5.place_forget()
     
+    export_type = None #['One Clip', 'Clip Sequence', 'Premiere', 'Davinci Resolve', 'Final Cut Pro', 'Shotcut']
+    if export5.get() == "One Clip":
+        export_type = None
+    elif export5.get() == "Clip Sequence":
+        export_type = "clip-sequence"
+    elif export5.get() == "Premiere":
+        export_type = "premiere"
+    elif export5.get() == "Davinci Resolve":
+        export_type = "resolve"
+    elif export5.get() == "Final Cut Pro":
+        export_type = "final-cut-pro"
+    elif export5.get() == "Shotcut":
+        export_type = "shotcut"
+
+    CutFile(file_path,export_type)
+
+    global progress_frame
+    progress_frame = CTkFrame(root, width = 700, height = 100, fg_color='transparent')
+    progress_frame.place(relx = 0.5, rely= 0.45, anchor= CENTER)
+    global progress_label
+    progress_label = CTkLabel(progress_frame, text="Cutting...", font = ("Aldo the Apache", 26))
+    progress_label.place(relx = 0.5, rely = 0.25, anchor = CENTER)
+    global progress_bar
+    progress_bar = CTkProgressBar(progress_frame, mode="indeterminate", progress_color='#3C9E6D')
+    progress_bar.start()  
+    progress_bar.place(relx = 0.5, rely = 0.6, anchor= CENTER)
+    root.after(1, stopbar, progress_frame, progress_bar)
+
+def mainmenu():
+    backtomenu.place_forget()
+    finish_label.place_forget()
+    progress_label.place_forget()
+    progress_frame.place_forget()
+    file_select_button.pack(side=LEFT,fill=BOTH,expand=True)
+    frame.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+def stopbar(progress_frame, progress_bar):
+    if CuttingProgress() == False:
+        return
     
+    global backtomenu
+    global finish_label
+    progress_bar.place_forget()
+    progress_label.configure(text = "Done!")
+    finish_label = CTkLabel(root,text = "File has been saved at the same directory of the selected file", font = (("Aldo the Apache", 20)))
+    finish_label.place(relx = 0.5, rely = 0.45, anchor = CENTER)
+    progress_label.configure(text_color = "#3C9E6D")
 
-def updateprogress():
-    progress = 0
+    image4 = CTkImage(light_image=Image.open("images/img14.png"), size=(30,30))
+    backtomenu = CTkButton(root, text= "Return to Main Menu", text_color='white', fg_color='transparent', font = ("Aldo the Apache", 26), image=image4, hover_color='#3C9E6D', command=mainmenu)
+    backtomenu.place(relx = 0.5, rely = 0.55, anchor = CENTER)
+    finished = False
 
-    def update():
-        nonlocal progress
-        progress += 5
-        Canvas.itemconfig(progressbar, width = progress)
-        if progress < 100:
-            Canvas.after(100, update)
-        else:
-            progress = 0
+canvas = CTkCanvas(root,bg="#242424",highlightthickness=0)
+canvas.pack(fill=BOTH,expand=TRUE)
+
+Namesize = CTkLabel(root, text = "", font = ("Aldo the Apache", 16))
+Namesize.place(relx = 0.5, rely = 0.45, anchor = "center")
+
+title_frame = CTkFrame(root,fg_color="transparent",bg_color="transparent")
+title_frame.place(relx=0.5,rely=0.3,anchor=CENTER)
+label = CTkLabel(title_frame,text="Silence ",font=("Aldo The Apache", 46), text_color='#e3e3e3')
+label2 = CTkLabel(title_frame,text="Remover",font=("Aldo The Apache", 46), text_color='#0099cc')
+label.pack(side=LEFT)
+label2.pack(side=LEFT)
 
 
-def move():
-    steps = 23
-    for i in range (steps + 1):
-        value = (i * steps) / 100
-        progressbar.set(value)
-        time.sleep(1)
+frame = CTkFrame(root,width=256,height=36,border_width=32,border_color="")
+frame.place(relx=0.5,rely=0.4,anchor=CENTER)
+frame.propagate(0)
 
-def updateonclick():
-    t = threading.Thread(target = move)
-    t.start()
+export5 = CTkComboBox(root, values= ['One Clip', 'Clip Sequence', 'Premiere', 'Davinci Resolve', 'Final Cut Pro', 'Shotcut'], width= 256, border_color="#484A48", button_color="#484A48", border_width=2, fg_color='#2C2C2C', 
+                     text_color='white', font= ("Aldo the Apache", 16), dropdown_font=("Aldo the Apache", 12), state= "readonly", hover=True)
+export5.set('One Clip')
+
+#export
+export5.lift(aboveThis=frame)
+export5.bind("<<ComboboxSelected>>")
+
+image2 = CTkImage(light_image= Image.open("images/img11.png"), size = (30,30))
+# file_entry = CTkEntry(frame,border_width=0)
+# file_entry.pack(side=LEFT,fill=BOTH,expand=True,anchor=CENTER)
+file_select_button = CTkButton(frame,text="Select file",fg_color="transparent",width=36, image = image2,
+                                font = ("Aldo The Apache", 18), text_color= 'white', command = select_file, hover_color='#5d91a2'
+                                , border_color='#859da8',border_width=float(0.7))
+file_select_button.pack(side=LEFT,fill=BOTH,expand=True)
+
+imagedel = CTkImage(light_image= Image.open("images/img13.png"), size = (25,25))
+deletebutton = CTkButton(master=frame, text="X", fg_color="transparent", width=36, text_color= 'white' ,
+                         hover_color='#9E3C3C', font=("Aldo The Apache", 18), command = delete_selection, image = imagedel
+                         ,border_width=1, border_color='#9E3C3C')
+
+previous_time = time()
+# image = Image.open("images/BG.png")
+# image = ImageTk.PhotoImage(image)
+speed = 30
+offset_x = 0
+offset_y = 0
+
+image3 = CTkImage(light_image= Image.open("images/img12.png"), size = (50,50))
+nextab = CTkButton(root, text = "Cut it!", image = image3, fg_color='transparent', width=16, 
+                   hover_color='#4b8b60', border_color='#3C9E6D', border_width=1, font = ("Aldo The Apache", 18), command= next_page)
+
+def process():
+    global previous_time, image, speed, offset_x, offset_y
+    canvas.delete('all')
     
-#progress bar
+    sine_points = [(0,root.winfo_height())]
+    for x in range(-8,root.winfo_width() + 8,8):
+        y = root.winfo_height() + round(8 * sin(radians(x + speed * time())))
+        pos = (x,y)
+        sine_points.append(pos)
 
-progressbar = ctk.CTkProgressBar(frame, orientation="horizontal")
-progressbar.place(relx = 0.5, rely = 0.80, anchor = "center")
-progressbar.set(0)
+    # for x in range(-64,root.winfo_width() + 64,64):
+    #     for y in range(-64,root.winfo_height() + 64,64):
+    #         canvas.create_image(x - offset_x,y + offset_y,image=image,anchor=NW)
 
+    canvas.create_line(sine_points,fill="#2e2e2e",width=64)
+    
+    dt = time() - previous_time
+    previous_time = time()
 
-#start button
-start = ctk.CTkButton(frame, text = "Start", command = updateonclick, font = font)
-start.place(relx = 0.5, rely = 0.7, anchor = "center")
+    offset_x += speed * dt
+    offset_y += speed * dt
 
+    if offset_x >= 64:
+        offset_x -= 64
+    elif offset_y >= 64:
+        offset_y -= 64
+    root.after(1,process)
 
-
-
-label = ctk.CTkLabel(frame, text = 'Welcome', font = font)
-label.pack()
-
-#choose file type
-Combobox = ctk.CTkComboBox(frame, state = "readonly", values=['Select export type','mp4', 'mp3'], width = 150, hover=True, button_hover_color='green')
-Combobox.place(relx = 0.5, rely = 0.55, anchor ="center")
-
-
-
-
-
-
-#test slider
-slider = ctk.CTkSlider(frame)
-slider.pack()
-window.mainloop()
-
-
-
-#img2 canceled
-#image2_path = os.path.join(os.path.dirname(__file__), 'images/bg.jpg')
-#image2 = ctk.CTkImage(light_image= Image.open(image2_path), size = (1024,720))
-#wndLab = ctk.CTkLabel(window, image = image2, text = "")
-#wndLab.pack()
+root.after(1,process)
+root.mainloop()
